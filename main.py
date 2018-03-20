a0a47be091f88c43d7a97c6301763e91ef3563a5
Esa Varemo <esa@kuivanto.fi>
2018-03-20 03:22:12 +0200

    Initialize SQLAlchemy

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import os
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URI']
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
