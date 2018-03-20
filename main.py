fcd6c64c3f5eeb507aa9375905e9706b3958c2b0
Esa Varemo <esa@kuivanto.fi>
2018-03-20 03:42:17 +0200

    Basic database model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -14,3 +15,23 @@ def hello_world():
 @app.route('/health')
 def healthcheck():
         return 'OK'
+
+class Link(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    short = db.Column(db.String(8), unique=True, nullable=False)
+    full  = db.Column(db.Text, nullable=False)
+
+    created       = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    requested     = db.Column(db.DateTime, default=None)
+    request_count = db.Column(db.Integer,  default=0)
+
+    @staticmethod
+    def request(short):
+        link = Link.query.filter_by(short=short)
+        if link:
+            link.requested = datetime.datetime.utcnow()
+            link.request_count += 1
+            return link.full
+        else:
+            return None
